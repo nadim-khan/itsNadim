c06e4a4b3a1e0f638f457c5f18d507514956aa66
nadim-khan <khan.nadim58@gmail.com>
2021-04-04 20:29:36 +0530

    CSS updated for collapse

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,20 @@
 */
 
 (function($) {
-
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
     var $window = $(window),
         $body = $('body'),
         $nav = $('#nav');
@@ -126,17 +139,3 @@ function openGit(url) {
     window.open(url);
 }
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
